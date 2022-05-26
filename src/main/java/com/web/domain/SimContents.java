e89c462b2272794f9c8fb23ac9600b4fd8d7e6a2
mingeun0507 <mkkim999@gmail.com>
2022-05-26 13:12:48 +0900

    fix: SimContents Entity pk 여러 개 준 것 수정

--- src/main/java/com/web/domain/SimContents.java
+++ src/main/java/com/web/domain/SimContents.java
@@ -18,63 +18,48 @@ public class SimContents {
     @Column(name = "CONTENT_ID", nullable = false)
     private Long id;
 
-    @Id
     @Column(name = "sim1")
     private String sim1;
 
-    @Id
     @Column(name = "sim2")
     private String sim2;
 
-    @Id
     @Column(name = "sim3")
     private String sim3;
 
-    @Id
     @Column(name = "sim4")
     private String sim4;
 
-    @Id
     @Column(name = "sim5")
     private String sim5;
 
-    @Id
     @Column(name = "sim6")
     private String sim6;
 
-    @Id
     @Column(name = "sim7")
     private String sim7;
 
-    @Id
     @Column(name = "sim8")
     private String sim8;
 
-    @Id
     @Column(name = "sim9")
     private String sim9;
 
-    @Id
     @Column(name = "sim10")
     private String sim10;
 
-    @Id
     @Column(name = "sim11")
     private String sim11;
 
-    @Id
     @Column(name = "sim12")
     private String sim13;
 
-    @Id
     @Column(name = "sim14")
     private String sim14;
 
-    @Id
     @Column(name = "sim15")
     private String sim15;
 
-    @Id
     @Column(name = "sim16")
     private String sim16;
 
