d73c9d6d928f1ed67a0ef291aa6a9bdedd4e4057
mingeun0507 <mkkim999@gmail.com>
2022-06-06 15:30:33 +0900

    fix: CORS 설정에서 AllowCredential 없앰

--- src/main/java/com/web/config/WebServiceConfig.java
+++ src/main/java/com/web/config/WebServiceConfig.java
@@ -12,7 +12,6 @@ public class WebServiceConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") //모든 요청에 대해서
-                .allowedOrigins("https://ee9a-219-255-158-172.jp.ngrok.io", "https://35cf-219-255-158-172.jp.ngrok.io", "http://localhost:5500", "http://10.21.20.18:5500")
-                .allowCredentials(true); //허용할 오리진들
+                .allowedOrigins("*"); //허용할 오리진들
     }
 }
